7360a636902b6f8bc942002ff41cf83883f08cd3
CSR_JR <53016847+cesarmironjr@users.noreply.github.com>
2023-10-07 14:30:52 -0600

    REPO 1 DESWEB PROYECTO

---
+++ app/Models/Alumno.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Alumno
+ *
+ * @property $id
+ * @property $nombre
+ * @property $telefono
+ * @property $correo
+ * @property $direccion
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Alumno extends Model
+{
+    
+    static $rules = [
+		'nombre' => 'required',
+		'telefono' => 'required',
+		'correo' => 'required',
+		'direccion' => 'required',
+    ];
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nombre','telefono','correo','direccion'];
+
+
+
+}
